befc460c3a59de960c06dcebf5ae0ff35f18f851
Rajaabpro <rajaab.pro@gmail.com>
2025-12-05 12:43:25 +0500

    Enhance addTodo reducer to generate unique IDs for new todos using nanoid

--- todo/src/features/todo/todoSlice.js
+++ todo/src/features/todo/todoSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
     todos: [{ id: 1, text: "Buy groceries" }],
@@ -9,7 +9,11 @@ export const tpdoSlice = createSlice({
     initialState,
     reducers:{
         addTodo: (state, action) => {
-            state.todos.push(action.payload);
+            const newTodo = {
+                id: nanoid(),
+                text: action.payload,
+            };
+            state.todos.push(newTodo);
         },
     }
 })
