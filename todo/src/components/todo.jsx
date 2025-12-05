2f40117abbcacced39842dd71660943e38c8be8f
Rajaabpro <rajaab.pro@gmail.com>
2025-12-05 15:06:14 +0500

    Implement todo list rendering in Todo component using Redux state

--- todo/src/components/todo.jsx
+++ todo/src/components/todo.jsx
@@ -1,7 +1,14 @@
+import { useSelector } from 'react-redux';
 export default function Todo() {
+    const todos = useSelector((state) => state.todo.todos);
     return (
       <>
-        <h3>Todos</h3>
+        <h3>Todos List</h3>
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>{todo.text}</li>
+            ))}
+        </ul>
       </>
     );
 }
