47787dcd94ef10c793975e98c26de75b8d4ea8d1
Rajaabpro <rajaab.pro@gmail.com>
2025-12-07 13:11:37 +0500

    Add click handler to delete button in Todo component for improved functionality

--- todo/src/components/todo.jsx
+++ todo/src/components/todo.jsx
@@ -1,13 +1,16 @@
 import { useSelector } from 'react-redux';
 export default function Todo() {
     const todos = useSelector((state) => state.todo.todos);
+    const clickHandler = (id) => {
+        console.log(id);
+    }
     return (
       <>
         <h3>Todos List</h3>
         <ul>
             {todos.map((todo) => (
                 <li key={todo.id}>{todo.text}
-                <button>Delete Task</button>
+                <button onClick={() => clickHandler(todo.id)}>Delete Task</button>
                 </li>
                 
             ))}
