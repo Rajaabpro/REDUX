b952618b7c161cacf687cc75d1b668f0767c93a0
Rajaabpro <rajaab.pro@gmail.com>
2025-12-07 13:06:15 +0500

    Refactor AddForm to use Redux for adding todos and streamline form submission

--- todo/src/components/AddForm.jsx
+++ todo/src/components/AddForm.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../features/todo/todoSlice';
 export default function AddForm() {
     const [task, setTask] = useState('');
-    const handleAddTask = () => {
-        console.log(task);
+    const dispatch = useDispatch();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(addTodo(task));
+        setTask('');
     }
     return (
         <>
-            <form>
+            <form onSubmit={submitHandler}>
                 <input type="text" placeholder="Add a new todo" value={task} onChange={(e) => setTask(e.target.value)} />
-                <button type="submit" onClick={handleAddTask}> Add Task</button>
+                <button type="submit"> Add Task</button>
             </form>
         </>
     )
